13a0600522e5a0734af9e71e8edea1be83698306
ysh <mysh212@gmail.com>
2026-08-13 16:47:42 +0800

    Update script to distinguish the problem ID
     - [ADD] Script to seperate different paragraph, question, group
     - [UPDATE] Dataset

--- dataset/init.py
+++ dataset/init.py
@@ -2,7 +2,7 @@
 import pandas as pd
 import requests
 
-import json
+import re, json
 
 rq = requests.Session()
 
@@ -27,10 +27,58 @@ while True:
         break
     ans = pd.concat([ans, tmp])
 
+ans = ans.reset_index()
 # %%
 ans.to_csv('ds.csv', index = False)
 
 # %%
+filter = re.compile(r'^SAT READING COMPREHENSION TEST\n*(.+)\n*Question (\d+):\n*(.+)\n*Answer:$', flags = re.MULTILINE | re.DOTALL)
 
+# %%
+filter.findall(ans.iloc[29].text)
+
+# %%
+ans['re'] = ans.text.map(lambda x: filter.findall(x))
+
+# %%
+ans['paragraph'] = ans.re.map(lambda x: x[0][0])
+ans['qid'] = ans.re.map(lambda x: x[0][1])
+ans['question'] = ans.re.map(lambda x: x[0][2])
+
+# %%
+id_filter = re.compile(r'^sat-practice_(\d+)-question_(\d+)$')
+
+# %%
+ans['re'] = ans.id.map(lambda x: id_filter.findall(x))
+
+ans['pid'] = ans.re.map(lambda x: x[0][0])
+ans['tqid'] = ans.re.map(lambda x: x[0][1])
+
+assert(all(ans.qid == ans.tqid))
+
+# %%
+ans['gid'] = pd.to_numeric(ans.pid)
+ans['qid'] = pd.to_numeric(ans.qid)
+
+# %%
+ans = ans.sort_values(['gid', 'qid'])
+ans = ans.drop(['re', 'tqid', 'index'], axis = 1)
+
+# %%
+ans = ans.reset_index(drop = True)
+
+# %%
+mapper = {i: j + 1 for j, i in enumerate(ans.paragraph.unique())}
+
+# %%
+ans['pid'] = ans.paragraph.map(lambda x: mapper[x])
+
+# %%
+assert len(ans.groupby('pid').paragraph.unique().map(lambda x: len(x)).unique()) == 1
+
+# %%
+
+# %%
+ans.to_csv('ds.csv', index = False)
 
 
